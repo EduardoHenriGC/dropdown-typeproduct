73887e52786de0c6de8edf017655f4bdc51ae517
eduardo henrique <eduardohenriqu3@hotmail.com>
2023-07-31 14:58:22 -0300

    css,components default and packages

---
+++ globalStyles.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    
+    
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+    
+    
+    
+  }
+  body{
+    color: #fff;
+    background-color: #333;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,9 @@
+import styles from "./navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <header className={styles.nav}>
+      <a href="">nav</a>
+    </header>
+  );
+}
